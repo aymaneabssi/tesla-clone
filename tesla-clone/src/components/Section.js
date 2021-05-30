9bf83726e2aa55b5e6b9fd7643d32c6a02b13294
aymaneabssi <78247123+aymaneabssi@users.noreply.github.com>
2021-05-30 23:54:33 +0200

    .

---
+++ tesla-clone/src/components/Section.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+const Section = () => {
+  return <Container>section</Container>;
+};
+
+export default Section;
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-image: url('/images/model-s.jpg');
+`;
